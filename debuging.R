eabb005d9a5c3804a80456294373f6e99aa236db
Jannik Buhr <jannik.m.buhr@gmail.com>
2018-08-24 13:26:36 +0200

    added heatmap, closes #11

--- debuging.R
+++ debuging.R
@@ -8,30 +8,29 @@
 input
 input %>% names()
 
+dev.off()
 names(df)
-
 df
 
-is.discrete(df["oh"])
-is.factor(df[[input$aes_facet1]])
 
-class(df[[input$aes_color]])
-
-input$aes_color
-input$aes_facet1
-
-
-names(df)
-
-dev.off()
-
-plt <- df %>%
-    ggplot()
-
-plt <- plt +
-    aes(x = !!sym(input$aes_x), y = !!sym(input$aes_y))+
-    geom_point()+
-    labs(title = "Alpha Version")
+plt <- ggplot(df) +
+    aes(x = !!sym(input$aes_x),
+        y = !!sym(input$aes_color),
+        fill = !!sym(input$aes_y)) +
+    geom_raster() +
+    mainTheme +
+    theme(
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        axis.text.y = element_text(size = input$heatLabSize, colour = "black"),
+        axis.title.y = element_text(size = 20),
+        axis.title.x = element_blank(),
+        plot.background = element_blank(),
+        panel.grid = element_blank()
+    ) +
+    scale_x_discrete(expand = c(0, 0)) +
+    scale_y_discrete(expand = c(0, 0)) +
+    scale_fill_viridis_c(option = input$heatColor, name = "mol%") +
+    NULL
 
 plt
 
