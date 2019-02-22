597b2c48770046fff2e7f266f32e1c0e0f7375d2
Jannik Buhr <jannik.m.buhr@gmail.com>
2019-02-22 16:09:12 +0100

    move context function down

--- run_tests.R
+++ run_tests.R
@@ -1,7 +1,7 @@
-context("Shiny Test")
 library(testthat)
 library(shinytest)
 
+context("Shiny Test")
 test_that("Application working?", {
     # Use compareImages=FALSE because the expected image screenshots were created
     # on a Mac, and they will differ from screenshots taken on the CI platform,
