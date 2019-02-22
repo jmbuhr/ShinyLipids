e66b93eac4a8e0bb6c59ed1b9558fcba04a9a01f
Jannik Buhr <jannik.m.buhr@gmail.com>
2019-02-22 16:00:49 +0100

    updating Test

--- run_tests.R
+++ run_tests.R
@@ -1,9 +1,11 @@
+context("Shiny Test")
 library(testthat)
 library(shinytest)
 
-test_that("Application works", {
+test_that("Application working?", {
     # Use compareImages=FALSE because the expected image screenshots were created
     # on a Mac, and they will differ from screenshots taken on the CI platform,
     # which runs on Linux.
     expect_pass(testApp(".", compareImages = FALSE))
 })
+
---
+++ tests/mytest.R
@@ -0,0 +1,16 @@
+app <- ShinyDriver$new("../")
+app$snapshotInit("mytest")
+
+# Input 'mainDataTable_rows_current' was set, but doesn't have an input binding.
+# Input 'mainDataTable_rows_all' was set, but doesn't have an input binding.
+# Input 'metaDataTable_rows_selected' was set, but doesn't have an input binding.
+# Input 'metaDataTable_row_last_clicked' was set, but doesn't have an input binding.
+# Input 'metaDataTable_cell_clicked' was set, but doesn't have an input binding.
+# Input 'mainDataTable_rows_current' was set, but doesn't have an input binding.
+# Input 'mainDataTable_rows_all' was set, but doesn't have an input binding.
+app$setInputs(tab = "main")
+# Input 'pairwiseComparisonsTable_rows_current' was set, but doesn't have an input binding.
+# Input 'pairwiseComparisonsTable_rows_all' was set, but doesn't have an input binding.
+app$setInputs(tab = "heatmap")
+app$setInputs(tab = "PCA")
+app$snapshot()
