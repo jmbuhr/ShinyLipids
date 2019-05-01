0678834260125d5900d3b2689ed1c31d1d6b05e1
Jannik Buhr <jannik.m.buhr@gmail.com>
2019-05-01 18:18:22 +0200

    adding new shinytest

--- tests/mytest.R
+++ tests/mytest.R
@@ -1,4 +1,4 @@
-app <- ShinyDriver$new("../", loadTimeout = 1e+05)
+app <- ShinyDriver$new("../", seed = 42)
 app$snapshotInit("mytest")
 
 # Input 'mainDataTable_rows_current' was set, but doesn't have an input binding.
@@ -8,4 +8,21 @@ app$setInputs(tab = "main")
 # Input 'pairwiseComparisonsTable_rows_all' was set, but doesn't have an input binding.
 app$setInputs(tab = "heatmap")
 app$setInputs(tab = "PCA")
+app$setInputs(tab = "main")
+app$snapshot()
+app$setInputs(ID = "2")
+app$setInputs(tab = "heatmap")
+app$setInputs(tab = "PCA")
+app$setInputs(tab = "heatmap")
+app$snapshot()
+app$setInputs(tab = "main")
+app$setInputs(tab = "metadata")
+# Input 'mainDataTable_rows_current' was set, but doesn't have an input binding.
+# Input 'mainDataTable_rows_all' was set, but doesn't have an input binding.
+app$setInputs(tab = "main")
+app$setInputs(main_add = c("points", "bars", "label"))
+app$setInputs(tab = "metadata")
+# Input 'mainDataTable_rows_all' was set, but doesn't have an input binding.
+# Input 'mainDataTable_rows_current' was set, but doesn't have an input binding.
+# Input 'mainDataTable_rows_all' was set, but doesn't have an input binding.
 app$snapshot()
