7a684451671d5366924277e1e0b356cc0741a64a
jannik <jannik.m.buhr@gmail.com>
2020-01-28 08:52:07 +0100

    add example database
    fix build
    
    remove test
    
    Need to find out why the database does not load when inside of a test
    fiix build
    
    Need to find out why the database does not load when inside of a test

--- tests/testthat/test-backend-functions.R
+++ tests/testthat/test-backend-functions.R
@@ -1,40 +1,40 @@
 context("Backend Functions")
 
+db_con <- DBI::dbConnect(RSQLite::SQLite(), "database/exampleDatabase.db")
 
+pkgload::load_all()
 
 test_that(
-  "Standardization for Species Profile of PC works as intended",
+  "Data processing steps run",
   {
-  # db_con <- DBI::dbConnect(RSQLite::SQLite(), dbname = "./database/exampleDatabase.db")
-  # 
-  # pkgload::load_all()
-  # 
-  # input <- list(
-  #   ID = 1,
-  #   custom_class_order = get_lipid_class_order(db_con),
-  #   std_tec_rep = TRUE,
-  #   std_feature = c("class", "sample_replicate"),
-  #   tecRep_average = TRUE,
-  #   base_sample = "",
-  #   filter_class = "PC",
-  #   aes_x = "lipid",
-  #   aes_facet1 = "class",
-  #   aes_facet2 = "",
-  #   aes_color = "sample"
-  # )
-  # 
-  # query <- sqlQueryData(input$ID)
-  # rawData <- collect_raw_data(db_con,
-  #                             query,
-  #                             custom_class_order = input$custom_class_order)
-  # 
-  # plotData <- rawData %>%
-  #   standardize_technical_replicates(input$std_tec_rep) %>% 
-  #   filter_rawData(input) %>% 
-  #   standardize_rawData(base_sample = input$base_sample,
-  #                       std_features = input$std_feature) %>% 
-  #   create_plotData(input)
-  # 
+  input <- list(
+    ID                 = 1,
+    custom_class_order = get_lipid_class_order(db_con),
+    std_tec_rep        = TRUE,
+    std_feature        = c("class", "sample_replicate"),
+    tecRep_average     = TRUE,
+    base_sample        = "",
+    filter_class       = "PC",
+    aes_x              = "lipid",
+    aes_facet1         = "class",
+    aes_facet2         = "",
+    aes_color          = "sample"
+  )
+
+  query <- sqlQueryData(input$ID)
+  rawData <- collect_raw_data(db_con,
+                              query,
+                              custom_class_order = input$custom_class_order)
+
+  plotData <- rawData %>%
+    standardize_technical_replicates(input$std_tec_rep) %>%
+    filter_rawData(input) %>%
+    standardize_rawData(base_sample  = input$base_sample,
+                        std_features = input$std_feature) %>%
+    create_plotData(input)
+  
+  expected <- c(0.4378872, 12.9379241, 7.1688470, 0.0213048, 3.1023356)
+  expect_equal(plotData$value[1:5], expected)
   }
 )
 
