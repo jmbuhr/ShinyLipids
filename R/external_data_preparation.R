068318022fbe6d84db234c88bcb3b97104c1607e
jannik <jannik.m.buhr@gmail.com>
2020-02-20 20:46:56 +0100

    remove unused function

--- R/external_data_preparation.R
+++ R/external_data_preparation.R
@@ -1,20 +1,16 @@
-utils::globalVariables(c("set", "name"))
+# utils::globalVariables(c("set", "name"))
 
-#' Read SDF file from Lipid Maps
-#'
-#' @param path string. File path to SDF file
-#'
-#' @return tibble. Tidy tibble of LM data
-readLipidMapsSDF <- function(path) {
-  if (!requireNamespace("ChemmineR", quietly = TRUE)) {
-    stop("Package \"ChemmineR\" needed for this function to work. Please install it.",
-         call. = FALSE)
-  } else {
-    ChemmineR::read.SDFset(path) %>% 
-      tibble::enframe(set@SDF) %>% 
-      mutate(body = map(value, "datablock")) %>% 
-      unnest_wider(body) %>% 
-      select(-value) %>% 
-      rename(ID = name)
-  }
-}
+## Not currently used
+# readLipidMapsSDF <- function(path) {
+#   if (!requireNamespace("ChemmineR", quietly = TRUE)) {
+#     stop("Package \"ChemmineR\" needed for this function to work. Please install it.",
+#          call. = FALSE)
+#   } else {
+#     ChemmineR::read.SDFset(path) %>% 
+#       tibble::enframe(set@SDF) %>% 
+#       mutate(body = map(value, "datablock")) %>% 
+#       unnest_wider(body) %>% 
+#       select(-value) %>% 
+#       rename(ID = name)
+#   }
+# }
